f652887775f6ae250b0f6141bb3b1e7fac526fca
Timothee Picard <timothee.picard@outlook.fr>
2022-12-10 18:25:45 +0100

    sort before format date without change file import

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -34,6 +34,7 @@ export default class {
       .list()
       .then(snapshot => {
         const bills = snapshot
+          .sort((a, b) => ((new Date(a.date) < new Date(b.date)) ? 1 : -1))
           .map(doc => {
             try {
               return {
@@ -52,7 +53,6 @@ export default class {
               }
             }
           })
-          console.log('length', bills.length)
         return bills
       })
     }
